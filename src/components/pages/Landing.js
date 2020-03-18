0b9f70f684acf239ae3380b147023e18e00da151
Crystal Ly <crystal.m.ly@gmail.com>
2020-03-18 10:33:27 -0500

    added buttons to Landing page

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-import SignIn from '../SignIn';
-import Profile from '../Profile';
 
 export default class Landing extends Component {
     render() {
         return (
             <div>
-                {/* Landing shows either SignIn page or Profile */}
+                {/* Landing shows a button to either SignIn page or SignUp */}
                 Landing page
-                <SignIn />
-                <Profile />
+                <button className="uk-button uk-button-secondary uk-button-large uk-margin-small-top uk-first-column">Sign In</button>
+
+                <button className="uk-button uk-button-secondary uk-button-large uk-margin-small-top uk-first-column">Sign Up</button>
             </div>
         )
     }
