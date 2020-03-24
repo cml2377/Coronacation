22447bdaf033b0f58dfc1fd7e6ecb64eddfb2d49
Crystal Ly <crystal.m.ly@gmail.com>
2020-03-24 12:48:45 -0500

    added SASS for findNeed and postNeed pages

--- client/src/components/pages/FindNeed.js
+++ client/src/components/pages/FindNeed.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 export default class FindNeed extends Component {
     render() {
         return (
-            <div>
-                This will display tiles of needs populated by database.
+            <div className="findNeedContainer">
+                <p>This will display tiles of needs populated by database.</p>
             </div>
         )
     }
--- client/src/components/pages/PostNeed.js
+++ client/src/components/pages/PostNeed.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 export default class PostNeed extends Component {
     render() {
         return (
-            <div>
-                Request form-- this is a list like the to-do list, with a bunch of items requested.
+            <div className="postNeedContainer">
+                <p>Request form-- this is a list like the to-do list, with a bunch of items requested.
 
-                When logged, the post will contain the user who requested the items, the items themselves, the general location (zipcode) and date created.
+                When logged, the post will contain the user who requested the items, the items themselves, the general location (zipcode) and date created.</p>
             </div>
         )
     }
